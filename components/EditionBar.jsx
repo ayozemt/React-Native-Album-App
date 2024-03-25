369c182b6be4a43728f1e5a556494ff8a387e431
ayozemt <ayozemt@gmail.com>
2024-03-25 13:29:26 +0100

    Implement status bar control

--- components/EditionBar.jsx
+++ components/EditionBar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
 
 const EditionBar = ({ navigation, onEditAlbum, showForm }) => {
+  const [showBar, setShowBar] = useState(false);
+  const toggleShowBar = () => {
+    setShowBar(!showBar);
+  };
+
   return (
     <View style={styles.toolbar}>
+      <StatusBar style="light" backgroundColor="grey" hidden={showBar} />
       <TouchableOpacity
         onPress={() => {
           navigation.navigate("Albums");
@@ -12,6 +19,14 @@ const EditionBar = ({ navigation, onEditAlbum, showForm }) => {
       >
         <Ionicons
           name="arrow-back"
+          size={24}
+          color="white"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={toggleShowBar}>
+        <Ionicons
+          name={showBar ? "chevron-down" : "chevron-up"}
           size={24}
           color="white"
           style={styles.icon}
