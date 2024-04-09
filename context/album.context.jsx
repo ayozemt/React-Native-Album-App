ccd4c2e09b267c2361a26831a4001195f25c94d3
ayozemt <ayozemt@gmail.com>
2024-04-09 13:22:22 +0200

    Implement permament storage

--- context/album.context.jsx
+++ context/album.context.jsx
@@ -1,10 +1,41 @@
-import React, { createContext, useState } from "react";
-import Albums from "../album-collection.json";
+import React, { createContext, useState, useEffect } from "react";
+// import Albums from "../album-collection.json";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AlbumContext = createContext();
 
 export const AlbumProvider = ({ children }) => {
-  const [albums, setAlbums] = useState(Albums);
+  const [albums, setAlbums] = useState([]);
+
+  const loadAlbumsFromStorage = async () => {
+    try {
+      const jsonAlbums = await AsyncStorage.getItem("albums");
+      if (jsonAlbums) {
+        setAlbums(JSON.parse(jsonAlbums));
+      } else {
+        setAlbums([]);
+      }
+    } catch (error) {
+      console.error("Error loading albums from AsyncStorage:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadAlbumsFromStorage();
+  }, []);
+
+  const saveAlbumsToStorage = async () => {
+    try {
+      const jsonAlbums = JSON.stringify(albums);
+      await AsyncStorage.setItem("albums", jsonAlbums);
+    } catch (error) {
+      console.error("Error saving albums to AsyncStorage:", error);
+    }
+  };
+
+  useEffect(() => {
+    saveAlbumsToStorage();
+  }, [albums]);
 
   return (
     <AlbumContext.Provider value={{ albums, setAlbums }}>
